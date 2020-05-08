a278fef414c338d1f913e21239643f23e6bd1b62
Xuan Hui(xuanhui919) <541951523@qq.com>
2020-05-08 22:49:46 +0800

    init email repository

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app-email" class="app-content--standalone app-email">
+    <p>邮件模块已启动</p>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
