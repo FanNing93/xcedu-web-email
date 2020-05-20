4d555d798595c87ccd858711deb89645dc9a87c7
Xuan Hui(xuanhui919) <541951523@qq.com>
2020-05-20 18:22:51 +0800

    change axios to xcedu-web-share

--- src/api/index.js
+++ src/api/index.js
@@ -1,21 +1,21 @@
-import request from './request'
+import { axios } from '@xcedu/web-share'
 // 修改提交数据
 export function updateEmail (form) {
-  return request.put('/api-mail/mail', form)
+  return axios.put('/api-mail/mail', form)
 }
 // 修改数据
 export function getEmailById (id) {
-  return request.get('/api-mail/mail/' + id)
+  return axios.get('/api-mail/mail/' + id)
 }
 // 查询列表
 export function getEmailList (params) {
-  return request.get('/api-mail/mail', params)
+  return axios.get('/api-mail/mail', params)
 }
 // 新建保存
 export function createEmail (form) {
-  return request.post('/api-mail/mail', form)
+  return axios.post('/api-mail/mail', form)
 }
 // 删除
 export function deleteEmailById (id) {
-  return request.delete('/api-mail/mail/' + id)
+  return axios.delete('/api-mail/mail/' + id)
 }
